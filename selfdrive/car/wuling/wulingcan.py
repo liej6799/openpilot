38aecbcd67e608243563e7cca924559d005ed2b6
Comma Device <device@comma.ai>
2023-05-01 20:35:40 +0700

    Update

---
+++ selfdrive/car/wuling/wulingcan.py
@@ -0,0 +1,87 @@
+import copy
+from cereal import car
+from common.numpy_fast import clip
+from selfdrive.car import make_can_msg
+
+VisualAlert = car.CarControl.HUDControl.VisualAlert
+
+def wuling_checksum(dat):
+  return sum(dat) & 0xFF
+
+def create_steering_control(packer, apply_steer, frame):
+
+  idx = (apply_steer) % 255
+  values = {
+      "STEER_TORQUE_CMD": clip(apply_steer,-100,100),
+      "SET_ME_X0": 0x00,
+      "COUNTER": (frame/2) % 4,
+      "STEER_REQUEST": 1 if apply_steer != 0 else 0,
+  }
+  values["COUNTER"] = (values["COUNTER"] + 1) % 0x11
+  
+  dat = packer.make_can_msg("STEERING_LKA", 0, values)[2]
+
+  crc = wuling_checksum(dat[:-1])
+  values["CHECKSUM"] = crc
+
+  return packer.make_can_msg("STEERING_LKA", 0, values)
+
+def create_steering_status(packer, apply_steer, frame, steer_step):
+  return packer.make_can_msg("ES_LKAS_State", 0, {})
+
+
+def create_acc_dashboard_command(packer, bus, acc_engaged, target_speed_kph, lead_car_in_sight, fcw):
+  # Not a bit shift, dash can round up based on low 4 bits.
+  target_speed = int(target_speed_kph * 16) & 0xfff
+
+  values = {
+    "ACCAlwaysOne" : 1,
+    "ACCResumeButton" : 0,
+    "ACCSpeedSetpoint" : target_speed,
+    "ACCGapLevel" : 3 * acc_engaged,  # 3 "far", 0 "inactive"
+    "ACCCmdActive" : acc_engaged,
+    "ACCAlwaysOne2" : 1,
+    "ACCLeadCar" : lead_car_in_sight,
+    "FCWAlert": 0x3 if fcw else 0
+  }
+
+  return packer.make_can_msg("ASCMActiveCruiseControlStatus", bus, values)
+
+
+def create_adas_time_status(bus, tt, idx):
+  dat = [(tt >> 20) & 0xff, (tt >> 12) & 0xff, (tt >> 4) & 0xff,
+    ((tt & 0xf) << 4) + (idx << 2)]
+  chksum = 0x1000 - dat[0] - dat[1] - dat[2] - dat[3]
+  chksum = chksum & 0xfff
+  dat += [0x40 + (chksum >> 8), chksum & 0xff, 0x12]
+  return make_can_msg(0xa1, bytes(dat), bus)
+
+def create_adas_steering_status(bus, idx):
+  dat = [idx << 6, 0xf0, 0x20, 0, 0, 0]
+  chksum = 0x60 + sum(dat)
+  dat += [chksum >> 8, chksum & 0xff]
+  return make_can_msg(0x306, bytes(dat), bus)
+
+def create_adas_accelerometer_speed_status(bus, speed_ms, idx):
+  spd = int(speed_ms * 16) & 0xfff
+  accel = 0 & 0xfff
+  # 0 if in park/neutral, 0x10 if in reverse, 0x08 for D/L
+  #stick = 0x08
+  near_range_cutoff = 0x27
+  near_range_mode = 1 if spd <= near_range_cutoff else 0
+  far_range_mode = 1 - near_range_mode
+  dat = [0x08, spd >> 4, ((spd & 0xf) << 4) | (accel >> 8), accel & 0xff, 0]
+  chksum = 0x62 + far_range_mode + (idx << 2) + dat[0] + dat[1] + dat[2] + dat[3] + dat[4]
+  dat += [(idx << 5) + (far_range_mode << 4) + (near_range_mode << 3) + (chksum >> 8), chksum & 0xff]
+  return make_can_msg(0x308, bytes(dat), bus)
+
+def create_adas_headlights_status(packer, bus):
+  values = {
+    "Always42": 0x42,
+    "Always4": 0x4,
+  }
+  return packer.make_can_msg("ASCMHeadlight", bus, values)
+
+def create_lka_icon_command(bus, active, critical, steer):
+  dat = b"\xc6\x3d\x01\x00\xac\x90\x02\x42"
+  return make_can_msg(0x104c006c, dat, bus)
