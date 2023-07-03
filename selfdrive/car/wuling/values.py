762e0518afa48028b85e57a76b9f0a13a7b4e172
Muammar Khadafi <daffigusti0890@gmail.com>
2023-07-03 17:00:44 +0700

    update wuling

---
+++ selfdrive/car/wuling/values.py
@@ -0,0 +1,122 @@
+from collections import defaultdict
+from dataclasses import dataclass
+from enum import Enum
+from typing import Dict, List, Union
+
+from cereal import car
+from selfdrive.car import dbc_dict
+from selfdrive.car.docs_definitions import CarFootnote, CarInfo, Column, Harness
+Ecu = car.CarParams.Ecu
+
+
+class CarControllerParams:
+
+  STEER_MAX = 350  # Safety limit, not LKA max. Trucks use 600.
+  STEER_STEP = 2  # control frames per command
+  STEER_DELTA_UP = 3      # 3 is stock. 100 is fine. 200 is too much it seems
+  STEER_DELTA_DOWN = 3    # no faults on the way down it seems
+  STEER_ERROR_MAX = 80
+  MIN_STEER_SPEED = 3.  # m/s
+
+  STEER_DRIVER_ALLOWANCE = 80
+  STEER_DRIVER_MULTIPLIER = 3    # weight driver torque heavily
+  STEER_DRIVER_FACTOR = 1        # from dbc
+  NEAR_STOP_BRAKE_PHASE = 0.5  # m/s
+  INACTIVE_STEER_STEP = 10  # Inactive control frames per command (10hz)
+  
+  # Heartbeat for dash "Service Adaptive Cruise" and "Service Front Camera"
+  ADAS_KEEPALIVE_STEP = 100
+  CAMERA_KEEPALIVE_STEP = 100
+  STEER_THRESHOLD = 60
+  HUD_MULTIPLIER = 0.685
+  
+  # Allow small margin below -3.5 m/s^2 from ISO 15622:2018 since we
+  # perform the closed loop control, and might need some
+  # to apply some more braking if we're on a downhill slope.
+  # Our controller should still keep the 2 second average above
+  # -3.5 m/s^2 as per planner limits
+  ACCEL_MAX = 2.  # m/s^2
+  ACCEL_MIN = -4.  # m/s^2
+
+  def __init__(self, CP):
+    # Gas/brake lookups
+    self.ZERO_GAS = 2048  # Coasting
+    self.MAX_BRAKE = 400  # ~ -4.0 m/s^2 with regen
+
+    self.MAX_GAS = 3072  # Safety limit, not ACC max. Stock ACC >4096 from standstill.
+    self.MAX_ACC_REGEN = 1404  # Max ACC regen is slightly less than max paddle regen
+    self.INACTIVE_REGEN = 1404
+    # ICE has much less engine braking force compared to regen in EVs,
+    # lower threshold removes some braking deadzone
+    max_regen_acceleration = 0.
+
+    self.GAS_LOOKUP_BP = [max_regen_acceleration, 0., self.ACCEL_MAX]
+    self.GAS_LOOKUP_V = [self.MAX_ACC_REGEN, self.ZERO_GAS, self.MAX_GAS]
+
+    self.BRAKE_LOOKUP_BP = [self.ACCEL_MIN, max_regen_acceleration]
+    self.BRAKE_LOOKUP_V = [self.MAX_BRAKE, 0.]
+    
+    assert(CP.lateralParams.torqueBP[0] == 0)
+    assert(CP.lateralParams.torqueBP[0] == 0)
+    self.STEER_LOOKUP_BP = [v * -1 for v in CP.lateralParams.torqueBP][1:][::-1] + list(CP.lateralParams.torqueBP)
+    self.STEER_LOOKUP_V = [v * -1 for v in CP.lateralParams.torqueV][1:][::-1] + list(CP.lateralParams.torqueV)
+    
+
+
+class CAR:
+ ALMAS_RS_PRO = "WULING ALMAZ RS PRO 2022"
+
+
+class Footnote(Enum):
+  OBD_II = CarFootnote(
+    'Wuling Almaz RS WITH Acc and LKAS',
+    Column.MODEL)
+
+
+@dataclass
+class GMCarInfo(CarInfo):
+  package: str = "Adaptive Cruise Control (ACC)"
+  harness: Enum = Harness.gm
+
+
+CAR_INFO: Dict[str, Union[GMCarInfo, List[GMCarInfo]]] = {
+  CAR.ALMAS_RS_PRO: GMCarInfo("Wuling Almaz RS Pro 2022"),
+}
+
+class CruiseButtons:
+  INIT = 0
+  UNPRESS = 1
+  RES_ACCEL = 2
+  DECEL_SET = 3
+  MAIN = 5
+  CANCEL = 6
+
+class AccState:
+  OFF = 0
+  ACTIVE = 1
+  FAULTED = 3
+  STANDSTILL = 4
+
+class CanBus:
+  POWERTRAIN = 0
+  OBSTACLE = 1
+  CAMERA = 2
+  CHASSIS = 2
+  SW_GMLAN = 3
+  LOOPBACK = 128
+  DROPPED = 192
+
+FINGERPRINTS = {
+  CAR.ALMAS_RS_PRO: [{
+    193: 8, 197: 8, 201: 8, 225: 8, 288: 5, 296: 8, 298: 8, 320: 4, 381: 8, 401: 8, 404: 8, 413: 8, 451: 8, 454: 8, 481: 8, 485: 8, 489: 8, 497: 8, 501: 8, 549: 8, 560: 8, 608: 8, 611: 8, 617: 8, 840: 6, 842: 6, 844: 6, 846: 6, 880: 8, 883: 8, 996: 8, 997: 8, 1041: 8, 1043: 8, 1045: 8, 1047: 8, 1053: 8, 1065: 8, 1217: 8, 1225: 8, 1341: 8, 1381: 8, 1406: 8, 1417: 8, 1538: 8, 1541: 8, 1543: 8, 1552: 8, 1569: 8
+  }]
+}
+
+FW_VERSIONS = {
+   CAR.ALMAS_RS_PRO: {}
+}
+DBC = {
+  CAR.ALMAS_RS_PRO: dbc_dict('wuling_almazrs_generated', None),
+}
+
+PREGLOBAL_CARS = []
---
+++ selfdrive/car/wuling/wulingcan.py
@@ -0,0 +1,139 @@
+import copy
+from cereal import car
+from common.numpy_fast import clip
+from selfdrive.car import make_can_msg
+
+VisualAlert = car.CarControl.HUDControl.VisualAlert
+
+def wuling_checksum(dat):
+  return sum(dat) & 0xFF
+
+def create_steering_control(packer, apply_steer, frame):
+
+  idx = (apply_steer) % 255
+  # apply_steer  = clip(apply_steer,-100,100);
+  values = {
+      "STEER_TORQUE_CMD": apply_steer,
+      "SET_ME_X0": 0x00,
+      "COUNTER": (frame/2) % 4,
+      "STEER_REQUEST": 1 if apply_steer != 0 else 0,
+  }
+  values["COUNTER"] = (values["COUNTER"] + 1) % 0x11
+  
+  dat = packer.make_can_msg("STEERING_LKA", 0, values)[2]
+
+  crc = wuling_checksum(dat[:-1])
+  values["CHECKSUM"] = crc
+
+  return packer.make_can_msg("STEERING_LKA", 0, values)
+
+def create_steering_status(packer, apply_steer, frame, steer_step):
+  return packer.make_can_msg("ES_LKAS_State", 0, {})
+
+
+def create_gas_command(packer,throttle, idx, acc_engaged, at_full_stop):
+  values = {
+    "ENABLE": acc_engaged,
+    "COUNTER": idx,
+    "COUNTER_2": idx,
+    "GAS_CMD": throttle,
+    "NEW_SIGNAL_2": 0x04,
+    "NEW_SIGNAL_1": 0x0b,
+  }
+
+  return packer.make_can_msg("GasCmd", 0, values)
+
+def create_brake_command(packer, apply_brake, idx, acc_engaged, at_full_stop):
+  values = {
+    "ENABLE": acc_engaged,
+    "COUNTER": idx,
+    "COUNTER_2": idx,
+    "BRAKE_CMD": apply_brake,
+    "BRAKE_CMD_2": 27,
+    "NEW_SIGNAL_8": 0x50,
+    "NEW_SIGNAL_5": 0x15,
+    "NEW_SIGNAL_4": 0x01,
+    "NEW_SIGNAL_2": 0x06,
+    "NEW_SIGNAL_6": 0x01,
+    "NEW_SIGNAL_3": 0x35,
+  }
+
+  return packer.make_can_msg("BRAKE_MODULE", 0, values)
+
+
+def create_acc_dashboard_command(packer, acc_engaged, idx, target_speed_kph, resume_button, lead_car_in_sight, fcw):
+  # Not a bit shift, dash can round up based on low 4 bits.
+  target_speed = int(target_speed_kph) & 0xfff
+
+  values = {
+    "ACCAlwaysOne" : 1,
+    "COUNTER_1" : idx,
+    "COUNTER_2" : (idx+1) % 4,
+    "ACCSTATE" : acc_engaged,
+    "ACCResumeButton" : resume_button,
+    "ACCSpeedSetpoint" : target_speed,
+    "ACCGapLevel" : 6 * acc_engaged,  # 3 "far", 0 "inactive"
+    "ACCCmdActive" : acc_engaged,
+    "ACCAlwaysOne2" : 3,
+    "ACCLeadCar" : lead_car_in_sight,
+    "SET_ME_X16" : 0x16,
+    "FCWAlert": 0x3 if fcw else 0
+  }
+
+  return packer.make_can_msg("ASCMActiveCruiseControlStatus", 0, values)
+
+
+def create_resume_cmd(packer, idx, resume):
+  values = {
+    "CRZ_BTN_1" : 1,
+    "CRZ_BTN_2" : 1,
+    "RESUME_BTN_1" : 1,
+    "RESUME_BTN_2" : 1,
+    "COUNTER_1" : idx,
+    "COUNTER_2" : idx,
+  }
+
+  return packer.make_can_msg("STEER_BTN", 0, values)
+
+def create_adas_time_status(bus, tt, idx):
+  dat = [(tt >> 20) & 0xff, (tt >> 12) & 0xff, (tt >> 4) & 0xff,
+    ((tt & 0xf) << 4) + (idx << 2)]
+  chksum = 0x1000 - dat[0] - dat[1] - dat[2] - dat[3]
+  chksum = chksum & 0xfff
+  dat += [0x40 + (chksum >> 8), chksum & 0xff, 0x12]
+  return make_can_msg(0xa1, bytes(dat), bus)
+
+def create_adas_steering_status(bus, idx):
+  dat = [idx << 6, 0xf0, 0x20, 0, 0, 0]
+  chksum = 0x60 + sum(dat)
+  dat += [chksum >> 8, chksum & 0xff]
+  return make_can_msg(0x306, bytes(dat), bus)
+
+def create_adas_accelerometer_speed_status(bus, speed_ms, idx):
+  spd = int(speed_ms * 16) & 0xfff
+  accel = 0 & 0xfff
+  # 0 if in park/neutral, 0x10 if in reverse, 0x08 for D/L
+  #stick = 0x08
+  near_range_cutoff = 0x27
+  near_range_mode = 1 if spd <= near_range_cutoff else 0
+  far_range_mode = 1 - near_range_mode
+  dat = [0x08, spd >> 4, ((spd & 0xf) << 4) | (accel >> 8), accel & 0xff, 0]
+  chksum = 0x62 + far_range_mode + (idx << 2) + dat[0] + dat[1] + dat[2] + dat[3] + dat[4]
+  dat += [(idx << 5) + (far_range_mode << 4) + (near_range_mode << 3) + (chksum >> 8), chksum & 0xff]
+  return make_can_msg(0x308, bytes(dat), bus)
+
+def create_adas_headlights_status(packer, bus):
+  values = {
+    "Always42": 0x42,
+    "Always4": 0x4,
+  }
+  return packer.make_can_msg("ASCMHeadlight", bus, values)
+
+def create_lka_icon_command(bus, active, critical, steer):
+  dat = b"\xc6\x3d\x01\x00\xac\x90\x02\x42"
+  return make_can_msg(0x104c006c, dat, bus)
+
+def create_resume_button(bus, active, critical, steer):
+  dat = b"\x48\x08\x00\x00\x00\x00\x00\x50"
+  return make_can_msg(0x1e1, dat, bus)
+
