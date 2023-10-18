adf58d1de5fa016b13840eb7c729aaddcc56fbde
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-10-18 09:55:38 -0700

    Experimental Mode via steering wheel / onroad UI
    Added toggle to enable or disable Experimental Mode from the steering wheel for Toyota/Lexus vehicles and the onroad UI for other makes.

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -295,6 +295,7 @@ void ui_update_params(UIState *s) {
   scene.custom_signals = scene.custom_theme ? params.getInt("CustomSignals") : 0;
 
   scene.developer_ui = params.getInt("DeveloperUI");
+  scene.experimental_mode_via_wheel = params.getBool("ExperimentalModeViaWheel");
   scene.screen_brightness = params.getInt("ScreenBrightness");
   scene.steering_wheel = params.getInt("SteeringWheel");
 }
