c75c9f42d452b889c5393f86bdce0aa90a266b0c
Joes Lie <liej6799@protonmail.ch>
2024-02-17 01:20:29 +0700

    test random address see if hit relay issue.

--- panda/board/safety/safety_wuling.h
+++ panda/board/safety/safety_wuling.h
@@ -59,7 +59,7 @@ static int wuling_rx_hook(CANPacket_t *to_push)
       pcm_cruise_check(cruise_engaged);
     }
 
-    //generic_rx_checks((addr == STEERING_LKAS) && (bus == 0));
+    generic_rx_checks((addr == 0xc9));
   }
 
   controls_allowed = 1;
