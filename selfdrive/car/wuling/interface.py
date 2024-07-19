4cc89cde86dfcea1eb17e4e024dc2fbe5f70e7e1
Joes Lie <liej6799@protonmail.ch>
2024-07-20 00:23:03 +0700

    does this help fix controls issue??

--- selfdrive/car/wuling/interface.py
+++ selfdrive/car/wuling/interface.py
@@ -129,7 +129,7 @@ class CarInterface(CarInterfaceBase):
     # print("Print cruise_button: ", self.CS.cruise_buttons)
     # print("Print prev_cruise_button: ", self.CS.prev_cruise_buttons)
   
-    events = self.create_common_events(ret, extra_gears=[GearShifter.sport, GearShifter.low, GearShifter.eco, GearShifter.manumatic], pcm_enable=False)
+    events = self.create_common_events(ret, extra_gears=[GearShifter.sport, GearShifter.low, GearShifter.eco, GearShifter.manumatic], pcm_enable=False, enable_buttons=(ButtonType.decelCruise,))
 
     # Enabling at a standstill with brake is allowed
     # TODO: verify 17 Volt can enable for the first time at a stop and allow for all GMs
