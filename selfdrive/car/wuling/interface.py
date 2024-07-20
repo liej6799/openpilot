8ec0e32064930ed9b2edc5b300e71116cdff666c
Joes Lie <liej6799@protonmail.ch>
2024-07-20 11:29:22 +0700

    enable back op

--- selfdrive/car/wuling/interface.py
+++ selfdrive/car/wuling/interface.py
@@ -132,8 +132,8 @@ class CarInterface(CarInterfaceBase):
   
     events = self.create_common_events(ret, extra_gears=[GearShifter.sport, GearShifter.low, GearShifter.eco, GearShifter.manumatic], pcm_enable=self.CP.pcmCruise, enable_buttons=(ButtonType.decelCruise,))
 
-    # if not self.CP.pcmCruise:
-    #   events.add(EventName.buttonEnable)
+    if not self.CP.pcmCruise:
+      events.add(EventName.buttonEnable)
 
     # Enabling at a standstill with brake is allowed
     # TODO: verify 17 Volt can enable for the first time at a stop and allow for all GMs
