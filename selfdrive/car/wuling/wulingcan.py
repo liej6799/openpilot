cea07d12e820cff3136b80a82138521ab285c470
Joes Lie <52598803+liej6799@users.noreply.github.com>
2024-08-17 23:12:54 +0700

    Update wulingcan.py

--- selfdrive/car/wuling/wulingcan.py
+++ selfdrive/car/wuling/wulingcan.py
@@ -38,13 +38,13 @@ def create_acc_command(packer, idx, acc_req):
   values = {
     "ALWAYS_ON_1": 5,
     "ALWAYS_ON_2": 0x78,
-    "GAS": 0, # change this later
-    "BRAKE": 1, #change this later
+    "GAS": 1, # change this later
+    "BRAKE": 0, #change this later
     "ACC_ACTIVE_1": 32 if acc_req else 0,
     "ACC_ACTIVE_2": 8 if acc_req else 0,
     "COUNTER": idx,
     "GAS_BRAKE_CMD": 4 if acc_req else 0, # need to check is it based on throttle.
-    "GAS_BRAKE_THROTTLE": 1696 # need to change later
+    "GAS_BRAKE_THROTTLE": 1900 # need to change later
   }
   
   values["COUNTER"] = (values["COUNTER"] + 1) % 0x11
