4bc5ce8c3fe9ba3c28e2edc9298a6f0f63b8fb85
Joes Lie <52598803+liej6799@users.noreply.github.com>
2024-08-22 18:46:05 +0700

    fix controller to be based on counter

--- panda/board/safety/safety_wuling.h
+++ panda/board/safety/safety_wuling.h
@@ -3,8 +3,7 @@
 #define ENGINE_DATA   0xc9
 #define LKAS_HUD      0x373
 #define STEERING_LKAS      0x225 // STEER 100%,
-#define BRAKE_DATA      0x269
-#define GAS_DATA      0x260 // USELESS STILL CAN BRAKE + GAS AFTER BLOCK
+#define BRAKE_DATA      0x260 // USELESS STILL CAN BRAKE + GAS AFTER BLOCK
 #define ACC_DATA      0x263 // BLOCK DASHBOARD SHOW CAMERA ERROR. CANT ENABLE ACC
 #define TEST_DATA_1   0x370 // BLOCK DASHBOARD SHOW CAMERA ERROR. CANT ENABLE ACC
 #define TEST_DATA_2   0x373 // DASHBAORD SHOW LKAS ERROR. STILL CAN ENABLE ACC
@@ -86,7 +85,7 @@ static int wuling_fwd_hook(int bus, int addr) {
   if (bus == BUS_MAIN) {
     bus_fwd = BUS_CAM;
   } else if (bus == BUS_CAM) {
-    bool block =  (addr == STEERING_LKAS || addr == GAS_DATA);
+    bool block =  (addr == STEERING_LKAS || addr == BRAKE_DATA);
     if (!block) {
       bus_fwd = BUS_MAIN;
     }
--- selfdrive/car/wuling/carcontroller.py
+++ selfdrive/car/wuling/carcontroller.py
@@ -8,19 +8,19 @@ from selfdrive.car.wuling import wulingcan
 from selfdrive.car.wuling.values import DBC, CanBus, PREGLOBAL_CARS, CarControllerParams
 from common.numpy_fast import clip
 
-LongCtrlState = car.CarControl.Actuators.LongControlState
-
 class CarController:
   def __init__(self, dbc_name, CP, VM):
     self.CP = CP
-    self.frame = 0
     
+    self.lka_steering_cmd_counter_last = -1
     self.apply_angle_last = 0
+    
+    self.frame = 0
+
     self.params = CarControllerParams(self.CP)
     
     self.packer_pt = CANPacker(DBC[self.CP.carFingerprint]['pt'])
-
-
+    
   def update(self, CC, CS, now_nanos):
     P = self.params
     
@@ -32,37 +32,18 @@ class CarController:
       apply_angle = apply_std_steer_angle_limits(actuators.steeringAngleDeg, self.apply_angle_last, CS.out.vEgo, CarControllerParams)
     else:
       apply_angle = CS.out.steeringAngleDeg
-  
+      
     self.apply_angle_last = apply_angle
     
-    if (self.frame % P.STEER_STEP) == 0:
+ 
+    if CS.lka_steering_cmd_counter != self.lka_steering_cmd_counter_last:
+      self.lka_steering_cmd_counter_last = CS.lka_steering_cmd_counter
+    elif (self.frame % P.STEER_STEP) == 0:
       lkas_enabled = CC.latActive
-        
-      idx = (self.frame/2) % 4
-      can_sends.append(wulingcan.create_steering_control(self.packer_pt, apply_angle, idx, lkas_enabled))
-   
-    # stopping = 0
-    # starting = 0    
-    # if CC.longActive:
-    #   long_enabled = True
-    #   accel = int(round(interp(actuators.accel, P.ACCEL_LOOKUP_BP, P.ACCEL_LOOKUP_V)))
-    #   if (actuators.longControlState == LongCtrlState.stopping):
-    #     stopping = 1
-    #   elif (actuators.longControlState == LongCtrlState.starting):
-    #     starting = 1
-        
-    # else:
-    #   long_enabled = False
-    #   accel = 0
-      
 
-    # print('long_enabled', long_enabled)
-    # print('Accel', accel)
-    # print('stopping', stopping)
-    # print('starting', starting)
+      idx = (self.frame/2) % 4
       
-      # DOnt send ACC first, need to figure out how to optimize the throttle. 
-      #can_sends.append(wulingcan.create_acc_command(self.packer_pt, idx, long_enabled, accel, starting, stopping))
+      can_sends.append(wulingcan.create_steering_control(self.packer_pt, apply_angle, idx, lkas_enabled))
     
     new_actuators = actuators.copy()
     new_actuators.steeringAngleDeg = apply_angle
