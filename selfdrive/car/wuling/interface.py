c11a6475717856d2423c36123d24f05ab1f7d2e4
Joes Lie <52598803+liej6799@users.noreply.github.com>
2024-08-22 18:57:28 +0700

    disable long

--- selfdrive/car/wuling/interface.py
+++ selfdrive/car/wuling/interface.py
@@ -49,7 +49,7 @@ class CarInterface(CarInterfaceBase):
     tire_stiffness_factor = 1  # Stock Michelin Energy Saver A/S, LiveParameters
     ret.centerToFront = ret.wheelbase * 0.4
     
-    ret.openpilotLongitudinalControl = True
+    ret.openpilotLongitudinalControl = False
 
     ret.steerLimitTimer = 1.0
     ret.steerActuatorDelay = 0.1
@@ -70,8 +70,8 @@ class CarInterface(CarInterfaceBase):
     ret.minSteerSpeed = 0 * CV.MPH_TO_MS
     
     params = Params()
-
-    ret.openpilotLongitudinalControl = True
+    if int(params.get("dp_atl").decode('utf-8')) == 1:
+      ret.openpilotLongitudinalControl = False
       
     ret.pcmCruise = False
   
