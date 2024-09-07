de2c2c0763e83085d918225651baf362f775eec4
Joes Lie <52598803+liej6799@users.noreply.github.com>
2024-09-07 12:41:25 +0700

    cleanup safety

--- panda/board/safety/safety_wuling.h
+++ panda/board/safety/safety_wuling.h
@@ -1,24 +1,31 @@
-// CAN msgs we care about
-//safety wuling
-#define ENGINE_DATA   0xc9
-#define LKAS_HUD      0x373
-#define STEERING_LKAS      0x225 // STEER 100%,
-#define BRAKE_DATA      0x260 // USELESS STILL CAN BRAKE + GAS AFTER BLOCK
-#define ACC_DATA      0x263 // BLOCK DASHBOARD SHOW CAMERA ERROR. CANT ENABLE ACC
-#define TEST_DATA_1   0x370 // BLOCK DASHBOARD SHOW CAMERA ERROR. CANT ENABLE ACC
-#define TEST_DATA_2   0x373 // DASHBAORD SHOW LKAS ERROR. STILL CAN ENABLE ACC
+#define WHEEL_DATA   0x348
+#define STEER_DATA   0x1e5
+#define ENGINE_DATA  0xc9
+#define GAS_DATA     0x191
+
+// Message from Camera
+#define STEERING_LKAS 0x225 // STEER
+#define BRAKE_DATA    0x260 // BRAKE
+#define ACC_DATA      0x263 // ACC
+#define TEST_DATA_1   0x370 // LKAS HUD 1
+#define TEST_DATA_2   0x373 // LKAS HUD 2
 
 // CAN bus numbers
 #define BUS_MAIN 0
 #define BUS_RADAR  1
 #define BUS_CAM  2
 
-const CanMsg WULING_TX_MSGS[] = {{ENGINE_DATA, 0, 8}, {LKAS_HUD, 0, 8}};
+const CanMsg WULING_TX_MSGS[] = {
+    {STEERING_LKAS, 0, 8}, 
+    {BRAKE_DATA, 0, 8},
+    {ACC_DATA, 0, 8}
+};
 
 AddrCheckStruct wl_addr_checks[] = {
-  // {.msg = {{ENGINE_DATA, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
-  // {.msg = {{BRAKE_DATA, 0, 8, .expected_timestep = 50000U}, { 0 }, { 0 }}},
-  // {.msg = {{GAS_DATA, 0, 8, .expected_timestep = 50000U}, { 0 }, { 0 }}},
+  {.msg = {{WHEEL_DATA,   0, 4, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{STEER_DATA,   0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{ENGINE_DATA,  0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{GAS_DATA,     0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}}
 };
 
 #define WL_RX_CHECK_LEN (sizeof(wl_addr_checks) / sizeof(wl_addr_checks[0]))
@@ -31,28 +38,24 @@ static int wuling_rx_hook(CANPacket_t *to_push) {
    if (valid && ((int)GET_BUS(to_push) == BUS_MAIN)) {
       int addr = GET_ADDR(to_push);
       // UNUSED(addr);
-      if (addr == 840) {
-        vehicle_moving = GET_BYTE(to_push, 0) | GET_BYTE(to_push, 1);
-      }
+      if (addr == WHEEL_DATA) {
+        int left_rear_speed = (GET_BYTE(to_push, 0) << 8) | GET_BYTE(to_push, 1);
+        int right_rear_speed = (GET_BYTE(to_push, 2) << 8) | GET_BYTE(to_push, 3);
+        vehicle_moving = (left_rear_speed > WL_STANDSTILL_THRESHOLD) || (right_rear_speed > WL_STANDSTILL_THRESHOLD);
+    }
 
-      if (addr == 485) {
-        int torque_driver_new = GET_BYTE(to_push, 6);
-        // update array of samples
-        update_sample(&torque_driver, torque_driver_new);
-       }
+      if (addr == STEER_DATA) {
+      int torque_driver_new = GET_BYTE(to_push, 6);
+      update_sample(&torque_driver, torque_driver_new);
+    }
       
-      if (addr == 201) {
+       if (addr == ENGINE_DATA) {
         brake_pressed = GET_BIT(to_push, 40U) != 0U;
-      }
+    }
 
-      if (addr == 0x191) {
+    if (addr == GAS_DATA) {
         gas_pressed = GET_BYTE(to_push, 6) != 0U;
-      }
-
-      // if ((addr == 0x263)) {
-      //   bool cruise_engaged = GET_BIT(to_push, 38U) != 0U;
-      //   pcm_cruise_check(cruise_engaged);
-      // }
+    }
 
       generic_rx_checks((addr == STEERING_LKAS));
    }
